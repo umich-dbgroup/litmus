1a06f613195f636a0097cadab22a199a7512269d
Chris Baik <chrisjbaik@gmail.com>
2018-08-16 13:45:42 -0400

    Add exhaustive method and rename files

--- main.py
+++ main.py
@@ -1,199 +1,195 @@
-from __future__ import division
-from moz_sql_parser import parse
+from __future__ import division, print_function
+
+import argparse
+from collections import OrderedDict
+from email.mime.text import MIMEText
 import json
-import mysql.connector
-import operator
+import smtplib
 import time
 
-def dist_tuple(t1, t2):
-    if q(t1) == q(t2):
-        return False
+# from moz_sql_parser import parse
+import mysql.connector
+from progress.bar import ChargingBar
 
-    match = 0
+def send_email(to_email, subject, message):
+    from_email = 'cannoliemailer@gmail.com'
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = to_email
 
-    # exclude the query id column from the check
-    exclude_id_len = len(t1) - 1
-    for i in range(0, exclude_id_len):
-        if t1[i] == t2[i]:
-            match += 1
-    return exclude_id_len - match == 1
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(from_email, 'cannoli123')
+    s.sendmail(from_email, [to_email], msg.as_string())
+    s.quit()
 
-def dist_set(T):
-    dist = 0
-    for i, t1 in enumerate(T):
-        for j in range(i+1, len(T)):
-            t2 = T[j]
-            if dist_tuple(t1, t2):
-                dist += 1
+def execute_query(conn, cq):
+    cursor = conn.cursor()
 
-    dist = dist * 2 / (len(T) * len(T))
-    return dist
+    try:
+        cursor.execute(cq)
 
-def q(t):
-    return t[len(t) - 1]
-
-def retrieve_tuples(conn, Q, filter):
-    U = []
-    print("Running queries...")
-    start = time.time()
-    for query_id, query in enumerate(Q):
-        parsed = parse(query)
-
-        projs = []
-        for sel in parsed['select']:
-            if 'distinct' in sel['value']:
-                projs.append(sel['value']['distinct'])
-            else:
-                projs.append(sel['value'])
-
-        filter_q = query
-        for i, filt_attr in enumerate(filter):
-            if filt_attr:
-                if isinstance(filt_attr, str):
-                    filter_q += " AND {} = '{}'".format(projs[i], filt_attr)
-                else:
-                    filter_q += " AND {} = {}".format(projs[i], filt_attr)
-
-        print("Running query {}...".format(filter_q))
-        cursor = conn.cursor()
-        cursor.execute(filter_q)
-
+        cq_tuples = set()
         for result in cursor:
-            result = result + (query_id,)
-            U.append(result)
-
-        print("Loaded {} tuples so far.".format(len(U)))
+            cq_tuples.add(result)
         cursor.close()
-    print("Done running queries. Elapsed time: {}s".format(time.time() - start))
-    return U
+        return cq_tuples
+    except Exception, exc:
+        cursor.close()
+        if str(exc).startswith('3024'):
+            raise Exception('Timeout: Query timed out.')
 
-def calc_pair_count(U):
-    print("Calculating dist pairs for each tuple...")
+def stats(conn, qid, cqs):
+    print("QUERY {}".format(qid))
     start = time.time()
-    D = {}
-    for i, t_i in enumerate(U):
-        if t_i not in D:
-            D[t_i] = 0
-        for j in range(i+1, len(U)):
-            t_j = U[j]
-            if dist_tuple(t_i, t_j):
-                D[t_i] += 1
-                if t_j not in D:
-                    D[t_j] = 0
-                D[t_j] += 1
-    print("Done calculating dist pairs for each tuple. Elapsed time: {}s".format(time.time() - start))
-    return D
 
-def sort_tuples_by_dist_pair_count(D):
-    print("Sorting tuples by dist pairs...")
+    tuples = {}
+    valid_cqs = 0
+    timed_out = 0
+
+    bar = ChargingBar('Running CQs', max=len(cqs), suffix='%(index)d/%(max)d (%(percent)d%%)')
+
+    for cq in cqs:
+        # print(cq)
+        # parsed = parse(cq)
+        #
+        # projs = []
+        # for sel in parsed['select']:
+        #     if 'distinct' in sel['value']:
+        #         projs.append(sel['value']['distinct'])
+        #     else:
+        #         projs.append(sel['value'])
+
+        try:
+            cq_tuples = execute_query(conn, cq)
+
+            if len(cq_tuples) > 0:
+                valid_cqs += 1
+
+            for t in cq_tuples:
+                if t in tuples:
+                    tuples[t] += 1
+                else:
+                    tuples[t] = 1
+        except Exception, exc:
+            if str(exc).startswith('Timeout'):
+                timed_out += 1
+
+        bar.next()
+
+    bar.finish()
+
+    print("Done executing CQs [{}s]".format(time.time() - start))
+
+    sorted_tuples = OrderedDict(sorted(tuples.items(), key=lambda t: t[1], reverse=True))
+
+    print('Total CQs: {}'.format(len(cqs)))
+    print('Timed Out CQs: {}'.format(timed_out))
+    print('Valid CQs: {}'.format(valid_cqs))
+    if valid_cqs > 0:
+        print('Avg. tuples per CQ: {}'.format(len(tuples)/valid_cqs))
+        k = 5
+        print('Tuples with most CQ overlap: ', end='')
+        for tuple, count in sorted_tuples.items()[0:k]:
+            print(count, end=', ')
+        print('\n')
+
+def dist(cqs, t, cqids):
+    Q_len = len(cqs)
+    C_t = len(cqids)
+    # TODO: when probabilities are unequal, p_t should be changed
+    p_t = C_t / Q_len
+
+    return ((Q_len - C_t) * p_t) + (C_t * (1 - p_t));
+
+def exhaustive(conn, qid, cqs):
+    print("QUERY {}: Exhaustive".format(qid))
     start = time.time()
-    D_sorted = sorted(D.items(), key=operator.itemgetter(1),reverse=True)
-    print("Done sorting tuples by dist pairs. Elapsed time: {}s".format(time.time() - start))
-    return D_sorted
 
-def add_highest_tuple_per_CQ(T,D_sorted,Q,U):
-    print("Getting tuple with highest dist pairs for each CQ...")
+    valid_cqs = []
+    timed_out = 0
+    tuples = {}
+
+    bar = ChargingBar('Running CQs', max=len(cqs), suffix='%(index)d/%(max)d (%(percent)d%%)')
+
+    for cq in cqs:
+        try:
+            cq_tuples = execute_query(conn, cq)
+
+            if len(cq_tuples) > 0:
+                cqid = len(valid_cqs)
+                valid_cqs.append(cq)
+
+                for t in cq_tuples:
+                    if t not in tuples:
+                        tuples[t] = []
+                    tuples[t].append(cqid)
+        except Exception, exc:
+            if str(exc).startswith('Timeout'):
+                timed_out += 1
+        bar.next()
+    bar.finish()
+    print("Done executing CQs [{}s]".format(time.time() - start))
+
+    print('Total CQs: {}'.format(len(cqs)))
+    print('Timed Out CQs: {}'.format(timed_out))
+    print('Valid CQs: {}'.format(len(valid_cqs)))
+
+    print("Calculating dist values...")
     start = time.time()
-    Q_r = set(range(0,len(Q)))     # remaining CQs
-    for (t,d) in D_sorted:
-        if q(t) in Q_r:
-            Q_r.discard(q(t))
-            T.append(t)
-            U.remove(t)
-        if not Q_r:
-            break
-    print("Found tuple with highest dist pair for each CQ. Elapsed time: {}s".format(time.time() - start))
+    tuple_dists = {}
+    for t, cqids in tuples.items():
+        tuple_dists[t] = dist(valid_cqs, t, cqids)
 
-def add_highest_tuple_for_CQ(T,D_sorted,query_id,U):
-    print("Getting tuple with highest dist pairs for CQ {}...".format(query_id))
-    start = time.time()
-    for (t,d) in D_sorted:
-        if q(t) == query_id:
-            T.append(t)
-            U.remove(t)
-            break
-    print("Found tuple with highest dist pair for CQ {}. Elapsed time: {}s".format(query_id, time.time() - start))
+    sorted_tuple_dists = OrderedDict(sorted(tuple_dists.items(), key=lambda t: t[1], reverse=True))
+    print("Done calculating dists [{}s]".format(time.time() - start))
+    k = 10
+    print("Top tuples:")
+    for t, dist_val in sorted_tuple_dists.items()[0:k]:
+        print("{}, Dist: {}, # CQs: {}".format(t, dist_val, len(tuples[t])))
+    print()
 
-def calc_pair_count_to_T(U,T):
-    print("Calculating dist pairs for remaining tuples to T...")
-    start = time.time()
-    D = {}
-    for t_i in U:
-        pair_count = 0
-        for t_j in T:
-            if dist_tuple(t_i, t_j):
-                pair_count += 1
-        D[t_i] = pair_count
-    print("Done calculating dist pairs for remaining tuples to T. Elapsed time: {}s".format(time.time() - start))
-    return D
-
-# filter is a tuple the same length as the projection
-#   - any None values means that attribute is not filtered
-def greedy_dist(conn, Q, k, filter):
-    U = retrieve_tuples(conn, Q, filter)
-    D = calc_pair_count(U)
-    D_sorted = sort_tuples_by_dist_pair_count(D)
-
-    T = []   # final set to return
-    add_highest_tuple_per_CQ(T,D_sorted,Q,U)
-
-    D_prime = calc_pair_count_to_T(U, T)
-    D_prime_sorted = sort_tuples_by_dist_pair_count(D_prime)
-
-    Y = []
-    for i in range(0,k-len(T)):
-        Y.append(D_prime_sorted[i][0])
-        U.remove(D_prime_sorted[i][0])
-
-    T = T + Y
-
-    print("Best k tuples; dist: {}".format(dist_set(T)))
-    for t in T:
-        print(t)
-
-    # with open('tuples.out', 'w') as f:
-    #     for (e1, e2, e3, interp) in T:
-    #         f.write("{}\t{}\t{}\t{}\n".format(e1.encode('utf-8'), e2.encode('utf-8'), e3, interp))
-
-def round_robin_dist(conn, Q, k, filter):
-    U = retrieve_tuples(conn, Q, filter)
-    D = calc_pair_count(U)
-    D_sorted = sort_tuples_by_dist_pair_count(D)
-
-    T = []   # final set to return
-    add_highest_tuple_per_CQ(T,D_sorted,Q,U)
-
-    q = 0
-    while len(T) < k:
-        D_prime = calc_pair_count_to_T(U, T)
-        D_prime_sorted = sort_tuples_by_dist_pair_count(D_prime)
-        add_highest_tuple_for_CQ(T,D_prime_sorted,q,U)
-
-        q += 1
-        if q == len(Q):
-            q = 0
-
-    print("Best k tuples; dist: {}".format(dist_set(T)))
-    for t in T:
-        print(t)
+def execute_mode(mode, conn, qid, cqs):
+    if mode == 'stats':
+        stats(conn, qid, cqs)
+    elif mode == 'exhaustive':
+        exhaustive(conn, qid, cqs)
 
 def main():
-    conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='imdb')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('db')
+    parser.add_argument('mode', choices=['stats', 'exhaustive'])
+    parser.add_argument('--qid', type=int)
+    parser.add_argument('--timeout', type=int, default=15000)
+    parser.add_argument('--email')
+    args = parser.parse_args()
 
-    Q = []
-    with open('queries.in', 'r') as f:
-        for line in f:
-            Q.append(line.strip())
+    conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database=args.db)
 
-    k = 10
+    # set query timeout
+    cursor = conn.cursor()
+    cursor.execute('SET SESSION MAX_EXECUTION_TIME={}'.format(args.timeout))
+    cursor.close()
 
-    # filter = ('Morgan Freeman', None, None, None)
-    # filter = (None, 'Steven Spielberg', None, None)
-    filter = ('Tom Cruise', None, None, None)
-    # greedy_dist(conn, Q, k, filter)
-    round_robin_dist(conn, Q, k, filter)
-    conn.close()
+    with open(args.db + '.json') as f:
+        data = json.load(f)
 
+    data = {int(k):v for k,v in data.items()}
+    sorted_data = OrderedDict(sorted(data.items(), key=lambda t: t[0]))
+
+    if args.qid is not None:
+        # if executing single query
+        execute_mode(args.mode, conn, args.qid, data[args.qid])
+    else:
+        # if executing all queries
+        for qid, cqs in sorted_data.items():
+            execute_mode(args.mode, conn, qid, cqs)
+
+    if args.email is not None:
+        send_email(args.email, 'Done {}'.format(args.db), 'Done')
 
 if __name__ == '__main__':
     main()
