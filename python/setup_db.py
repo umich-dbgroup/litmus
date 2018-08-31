f725cec54284046dbe669e8f3100a588e846116a
Chris Baik <chrisjbaik@gmail.com>
2018-08-31 11:48:23 -0400

    add setup db to cleanup indexes

---
+++ python/setup_db.py
@@ -0,0 +1,68 @@
+import argparse
+import ConfigParser
+
+from utils.database import Database
+
+TEXT_INDEX_SIZE = 15
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('db')
+    args = parser.parse_args()
+
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.read('config.ini')
+
+    db = Database(config.get('database', 'user'), config.get('database', 'pw'), config.get('database', 'host'), args.db, config.get('database', 'cache_path'), timeout=config.get('database', 'timeout'))
+
+    rels = db.get_relations()
+    for relname, rel in rels.items():
+        # find non-unique indexes
+        cursor = db.cursor()
+        cursor.execute('SHOW INDEXES FROM {}'.format(relname))
+        index_rows = cursor.fetchall()
+        cursor.close()
+
+        # drop all indexes
+        unique_keys = []
+        for row in index_rows:
+            non_unique = row[1]
+            index_name = row[2]
+            column_name = row[4]
+            if non_unique == 1:
+                cursor = db.cursor()
+                qstr = 'ALTER TABLE {} DROP INDEX {}'.format(relname, index_name)
+                print(qstr)
+                cursor.execute(qstr)
+                cursor.close()
+            else:
+                unique_keys.append(column_name)
+
+        # add all needed indexes for each attribute
+        for attr_name, attr in rel.attrs.items():
+            if attr_name in unique_keys:
+                continue
+
+            if attr.type == 'num':
+                cursor = db.cursor()
+                qstr = 'ALTER TABLE {} ADD INDEX {}({})'.format(relname, attr_name, attr_name)
+                print(qstr)
+                cursor.execute(qstr)
+                cursor.close()
+            elif attr.type == 'text':
+                cursor = db.cursor()
+                qstr = 'ALTER TABLE {} ADD INDEX {}({}({}))'.format(relname, attr_name, attr_name, TEXT_INDEX_SIZE)
+                print(qstr)
+                cursor.execute(qstr)
+                cursor.close()
+
+                cursor = db.cursor()
+                qstr = 'ALTER TABLE {} ADD FULLTEXT {}_ft({})'.format(relname, attr_name, attr_name)
+                print(qstr)
+                cursor.execute(qstr)
+                cursor.close()
+
+
+
+if __name__ == '__main__':
+    main()
