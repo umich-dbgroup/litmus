250bb84acbd373479adade05dddc95fbfa4ede36
Chris Baik <chrisjbaik@gmail.com>
2018-09-05 15:43:19 -0400

    update find_spj for new format

--- python/utils/find_spj.py
+++ python/utils/find_spj.py
@@ -18,7 +18,7 @@ def find_excludes(db):
     data = OrderedDict(sorted(data.items(), key=lambda x: x[0]))
     excludes = []
     for qid, task in data.items():
-        for cq in task['cqs']:
+        for cqid, cq in task['cqs'].items():
             if any(w in cq for w in NON_SPJ_WORDS):
                 excludes.append(int(qid))
                 break
