f9330b75dafa2c2c720df72054f00d0a98912bf9
cjbaik <chrisjbaik@gmail.com>
2018-10-26 17:12:51 -0400

    Rename systems in run many script

--- python/run_many.sh
+++ python/run_many.sh
@@ -8,11 +8,11 @@ python -u main.py $db greedyall --email=chrisjbaik@gmail.com | tee ${db}_greedya
 rm cache/${db}_greedybb_range.pkl
 python -u main.py $db greedybb --info=range --email=chrisjbaik@gmail.com | tee ${db}_greedybb.out
 
-rm cache/${db}_greedyguess_range.pkl
-python -u main.py $db greedyguess --info=range --email=chrisjbaik@gmail.com | tee ${db}_greedyguess.out
+rm cache/${db}_greedyfirst_range.pkl
+python -u main.py $db greedyfirst --info=range --email=chrisjbaik@gmail.com | tee ${db}_greedyfirst.out
 
 for i in {1..5}
 do
-  rm cache/${db}_random.pkl
-  python -u main.py $db random --email=chrisjbaik@gmail.com | tee ${db}_random.out
+  rm cache/${db}_gav.pkl
+  python -u main.py $db gav --email=chrisjbaik@gmail.com | tee ${db}_gav.out
 done
