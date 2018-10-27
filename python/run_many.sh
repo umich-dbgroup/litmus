111b796a0ee7cc836e0b6f6d90eb51a09531dd3f
cjbaik <chrisjbaik@gmail.com>
2018-10-27 13:21:00 -0400

    Update run many script to do each 5 times

--- python/run_many.sh
+++ python/run_many.sh
@@ -2,6 +2,8 @@
 
 db=$1
 
+for i in {1..5}
+do
 rm cache/${db}_greedyall.pkl
 python -u main.py $db greedyall --email=chrisjbaik@gmail.com | tee ${db}_greedyall.out
 
@@ -11,8 +13,6 @@ python -u main.py $db greedybb --info=range --email=chrisjbaik@gmail.com | tee $
 rm cache/${db}_greedyfirst_range.pkl
 python -u main.py $db greedyfirst --info=range --email=chrisjbaik@gmail.com | tee ${db}_greedyfirst.out
 
-for i in {1..5}
-do
-  rm cache/${db}_gav.pkl
-  python -u main.py $db gav --email=chrisjbaik@gmail.com | tee ${db}_gav.out
+rm cache/${db}_gav.pkl
+python -u main.py $db gav --email=chrisjbaik@gmail.com | tee ${db}_gav.out
 done
