ab3ee0d2083f3aeae4899158babe9db826604dc9
Chris Baik <chrisjbaik@gmail.com>
2018-10-30 14:11:46 -0400

    Add clean_cqs script

---
+++ python/clean_cqs.py
@@ -0,0 +1,96 @@
+import argparse
+import json
+import operator
+import time
+
+from collections import OrderedDict
+import ConfigParser
+from tqdm import tqdm
+
+from modules.database import Database
+from modules.excludes import find_excludes
+from modules.mailer import Mailer
+from modules.parser import SQLParser
+from tqc import create_temp_table_for_tq, get_tq_count, cq_tq_intersects
+
+MAX_Q_TIME = 900    # 900s = 15 min
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('db')
+    argparser.add_argument('--email')
+    args = argparser.parse_args()
+
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.read('config.ini')
+
+    db = Database(config.get('database', 'user'), config.get('database', 'pw'), config.get('database', 'host'), args.db, config.get('database', 'cache_dir'), timeout=config.get('database', 'timeout'), buffer_pool_size=config.get('database', 'buffer_pool_size'))
+
+    parser = SQLParser(args.db, config.get('parser', 'cache_dir'))
+
+    data = json.load(open('../data/{}.json'.format(args.db), 'r'))
+    data = OrderedDict(sorted(data.items(), key=lambda x: x[0]))
+
+    excludes = find_excludes(args.db)
+
+    for qid, task in data.items():
+        print('\nExamining Task {}...'.format(qid))
+        if qid in excludes:
+            print('Skipping excluded query.')
+            continue
+
+        assert(len(task['ans']) == 1)
+
+        tqid = task['ans'][0]
+
+        tq, cached = parser.parse_one(qid, tqid, task['cqs'][tqid])
+        tq_types = create_temp_table_for_tq(db, tq)
+        tq_count = get_tq_count(db)
+
+        cq_infos = []  # list of (cqid, negative of query time, intersects w/tq)
+        bar = tqdm(total=len(task['cqs']), desc='Checking CQs')
+        for cqid, cq_str in task['cqs'].items():
+            cq, cached = parser.parse_one(qid, cqid, cq_str)
+
+            start = time.time()
+            try:
+                tups = db.execute_sql(cq.query_str)
+            except Exception as e:
+                if not str(e).startswith('Timeout'):
+                    raise e
+            q_time = time.time() - start
+
+            x_count = cq_tq_intersects(db, cqid, tqid, cq, tq, tq_count, tq_types)
+
+            cq_infos.append((cqid, -1 * q_time, x_count))
+            bar.update(1)
+        bar.close()
+
+        cq_infos.sort(key=operator.itemgetter(2, 1))
+
+        total_q_time = sum(-1 * i[1] for i in cq_infos)
+        while total_q_time > MAX_Q_TIME:
+            cur_cq_info = cq_infos.pop(0)
+            print('Removing CQ {}, query_time: {}, intersects: {}'.format(cur_cq_info[0], -cur_cq_info[1], cur_cq_info[2]))
+            total_q_time += cur_cq_info[1]
+
+        cursor = db.cursor()
+        cursor.execute('DROP TABLE tq')
+        cursor.close()
+
+        # remaining CQs should be saved back into data
+        new_cqs = {}
+        for i in cq_infos:
+            new_cqs[i[0]] = task['cqs'][i[0]]
+        task['cqs'] = OrderedDict(sorted(new_cqs.items(), key=lambda x: x[0]))
+
+    data = OrderedDict(sorted(data.items(), key=lambda x: x[0]))
+
+    json.dump(open('../data/{}.cleaned.json'.format(args.db), 'w'))
+
+    if args.email is not None:
+        mailer = Mailer()
+        mailer.send(args.email, 'Done cleaning CQs for {}'.format(args.db), 'Done')
+
+if __name__ == '__main__':
+    main()
--- python/tqc.py
+++ python/tqc.py
@@ -21,18 +21,7 @@ def tqc(ans_count, intersect_counts):
 
     return 1 - (1 / denom)
 
-def tqc_for_task(db, parser, qid, task):
-    assert(len(task['ans']) == 1)
-
-    if len(task['cqs']) == 1:
-        print('Only one CQ.')
-        return 0
-
-    ans_id = task['ans'][0]
-    tq_str = task['cqs'][ans_id]
-    tq, cached = parser.parse_one(qid, ans_id, tq_str)
-
-    # save target query to temp table
+def create_temp_table_for_tq(db, tq):
     print('Creating temporary table...')
     tq_types = ()
     tmp = 'CREATE TEMPORARY TABLE tq {}'.format(tq.query_str)
@@ -45,57 +34,70 @@ def tqc_for_task(db, parser, qid, task):
     cursor.execute(tmp)
     cursor.close()
     print('Done creating temporary table.')
+    return tq_types
 
-    # count how many results in answer
+def get_tq_count(db):
     cursor = db.cursor()
     cursor.execute('SELECT COUNT(*) FROM tq')
     row = cursor.fetchone()
     cursor.close()
-    ans_count = row[0]
-    print('TQ Count: {}'.format(ans_count))
+    return row[0]
+
+def cq_tq_intersects(db, cqid, tqid, cq, tq, tq_count, tq_types):
+    if cqid == tqid:
+        return tq_count
+
+    check = re.sub('(SELECT) (DISTINCT )?(.*) (FROM)', '\g<1> /*+ MAX_EXECUTION_TIME(100000) */ COUNT(DISTINCT \g<3>) \g<4> tq,', cq.query_str, count=1)
+    if 'where' not in check.lower():
+        check += ' WHERE '
+    else:
+        check += ' AND '
+    check_constraints = []
+    cq_types = ()
+    for i, proj in enumerate(cq.projs):
+        check_constraints.append('tq.p{} = {}'.format(i, proj))
+        attr = db.get_attr(proj)
+        cq_types = cq_types + (attr.type,)
+    check += ' AND '.join(check_constraints)
+
+    if cq_types != tq_types:
+        return 0
+
+    cursor = db.cursor()
+    try:
+        cursor.execute(check)
+        row = cursor.fetchone()
+        cursor.close()
+        return row[0]
+    except Exception as e:
+        if str(e).startswith('3024'):
+            cursor.close()
+            return 0
+        else:
+            raise e
+
+def tqc_for_task(db, parser, qid, task):
+    assert(len(task['ans']) == 1)
+
+    if len(task['cqs']) == 1:
+        print('Only one CQ.')
+        return 0
+
+    ans_id = task['ans'][0]
+    tq_str = task['cqs'][ans_id]
+    tq, cached = parser.parse_one(qid, ans_id, tq_str)
+
+    tq_types = create_temp_table_for_tq(db, tq)
+    ans_count = get_tq_count(db)
 
     intersect_counts = {}
 
     bar = tqdm(total=len(task['cqs']), desc='Calc intersects')
 
     for cqid, cq_str in task['cqs'].items():
-        if cqid == ans_id:
-            intersect_counts[cqid] = ans_count
-            bar.update(1)
-            continue
-
         cq, cached = parser.parse_one(qid, cqid, cq_str)
 
-        check = re.sub('(SELECT) (DISTINCT )?(.*) (FROM)', '\g<1> COUNT(DISTINCT \g<3>) \g<4> tq,', cq_str, count=1)
-        if 'where' not in check.lower():
-            check += ' WHERE '
-        else:
-            check += ' AND '
-        check_constraints = []
-        cq_types = ()
-        for i, proj in enumerate(cq.projs):
-            check_constraints.append('tq.p{} = {}'.format(i, proj))
-            attr = db.get_attr(proj)
-            cq_types = cq_types + (attr.type,)
-        check += ' AND '.join(check_constraints)
-
-        if cq_types != tq_types:
-            intersect_counts[cqid] = 0
-            bar.update(1)
-            continue
-
-        cursor = db.cursor()
-        try:
-            cursor.execute(check)
-            row = cursor.fetchone()
-            cursor.close()
-            intersect_counts[cqid] = row[0]
-        except Exception as e:
-            if str(e).startswith('3024'):
-                cursor.close()
-                intersect_counts[cqid] = 0
-            else:
-                raise e
+        intersect_counts[cqid] = cq_tq_intersects(db, cqid, ans_id, cq, tq, ans_count, tq_types)
         bar.update(1)
     bar.close()
 
@@ -132,8 +134,7 @@ def main():
     config = ConfigParser.RawConfigParser(allow_no_value=True)
     config.read('config.ini')
 
-    timeout = 100000
-    db = Database(config.get('database', 'user'), config.get('database', 'pw'), config.get('database', 'host'), args.db, config.get('database', 'cache_dir'), timeout=timeout, buffer_pool_size=config.get('database', 'buffer_pool_size'))
+    db = Database(config.get('database', 'user'), config.get('database', 'pw'), config.get('database', 'host'), args.db, config.get('database', 'cache_dir'), timeout=config.get('database', 'timeout'), buffer_pool_size=config.get('database', 'buffer_pool_size'))
     parser = SQLParser(args.db, config.get('parser', 'cache_dir'))
 
     tasks = load_tasks(config.get('main', 'data_dir'), args.db)
