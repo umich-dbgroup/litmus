a8dbd0963efb708456f11c47b5ce70aeb53baea9
Chris Baik <chrisjbaik@gmail.com>
2018-11-02 16:39:40 -0400

    open logging in unbuffered mode

--- python/modules/logger.py
+++ python/modules/logger.py
@@ -4,7 +4,7 @@ class Logger:
     def __init__(self, path):
         self._path = path
     def __enter__(self):
-        sys.stdout = open(self._path, mode='w')
+        sys.stdout = open(self._path, 'w', 0)
         return self
     def __exit__(self, exec_type, exc_val, exc_tb):
         sys.stdout.close()
